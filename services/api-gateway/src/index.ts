5d6d024bd03ef0faca617133f0e56fa55d1362e2
athulraj2027 <athulraj2027@gmail.com>
2026-03-19 09:21:25 +0530

    gateway:added location service

--- services/api-gateway/src/index.ts
+++ services/api-gateway/src/index.ts
@@ -10,21 +10,24 @@ import routes from "./routes/index.routes.js";
 const PORT = process.env.PORT || 3005;
 const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL;
 const BUS_SERVICE_URL = process.env.BUS_SERVICE_URL;
+const LOCATION_SERVICE_URL = process.env.LOCATION_SERVICE_URL;
 
 const app = express();
 app.use(cors());
 
 app.use(
   "/auth",
-  createProxyMiddleware({
-    target: AUTH_SERVICE_URL,
-    changeOrigin: true,
-  }),
+  createProxyMiddleware({ target: AUTH_SERVICE_URL, changeOrigin: true }),
 );
 
 app.use(
   "/bus",
   createProxyMiddleware({ target: BUS_SERVICE_URL, changeOrigin: true }),
+);
+
+app.use(
+  "/location",
+  createProxyMiddleware({ target: LOCATION_SERVICE_URL, changeOrigin: true }),
 );
 
 app.use("/", routes);
